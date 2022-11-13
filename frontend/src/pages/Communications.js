39d502ff4a700840f50dc3f9cfa1a78c3a8802c0
Kmerge Boi <kpdosdos@gmail.com>
2022-11-13 20:35:15 +0800

    feat: communications page

--- frontend/src/pages/Communications.js
+++ frontend/src/pages/Communications.js
@@ -1,8 +1,15 @@
+import { useState } from 'react';
+import DataTable from '../components/DataTable';
+import COMMUNICATIONS from '../fake-api/COMMUNICATIONS.json';
+
 
 const Communications = () => {
+
+   const [communicationsData, setCommunicationsData] = useState(COMMUNICATIONS);
+
    return (
       <div>
-         Commuincations
+         <DataTable data={communicationsData} />
       </div>
    );
 }

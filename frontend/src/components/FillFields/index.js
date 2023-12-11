286d382e7bb7c09c94a574b7d4dd8235432ec007
Kmerge Boi <kpdosdos@gmail.com>
2023-12-11 21:15:31 +0800

    init nextjs app

--- frontend/src/components/FillFields/index.js
+++
@@ -1,9 +0,0 @@
-import './index.css';
-
-const FillFields = () => {
-   return (
-      <div>FillFields Content</div>
-   );
-}
-
-export default FillFields;
--- frontend/src/pages/FillFields.js
+++
@@ -1,8 +0,0 @@
-
-const FillFields = () => {
-  return (
-    <div>FillFields</div>
-  );
-}
-
-export default FillFields;
